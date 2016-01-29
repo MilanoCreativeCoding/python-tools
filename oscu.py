0f4157937c7fd531b6d90d64cc428fc0984d73c2
Edoardo Negri <edne@gmx.com>
2016-01-29 14:27:48 +0100

    Forward messages

--- oscu.py
+++ oscu.py
@@ -6,13 +6,13 @@ import liblo
 
 
 def print_log(port=1234):
-    "Display every message received on the given port (optional)"
+    "Display every message received on the given port (default 1234)"
 
     print("Logging port:", port)
     print()
 
-    def callback(path, args):
-        print(path, *args)
+    def callback(path, msg):
+        print(path, *msg)
 
     server = liblo.ServerThread(int(port))
     server.add_method(None, None, callback)  # wildcard callback
@@ -31,6 +31,27 @@ def send(addr, path, *msg):
     liblo.send(target, path, *msg)
 
 
+def forward(out_addr, in_port=1234):
+    "Forward received messages: out_ip:out_port in_port"
+
+    print("Forwarding  port:", in_port, "to", out_addr)
+    print()
+
+    def callback(path, msg):
+        print(path, *msg)
+        send(out_addr, path, *msg)
+
+    server = liblo.ServerThread(int(in_port))
+    server.add_method(None, None, callback)  # wildcard callback
+    server.start()
+
+    try:
+        while True:
+            sleep(0.1)
+    except KeyboardInterrupt:
+        server.stop()
+
+
 def main():
     "Main function, used when called as script"
     commands = {}
@@ -44,6 +65,7 @@ def main():
 
     commands["log"] = print_log
     commands["send"] = send
+    commands["forward"] = forward
     commands["help"] = print_help
 
     if not argv[1:]:
